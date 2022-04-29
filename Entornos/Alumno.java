f6b7c868788b952e69bb0f757e5342972ee1409b
IkerQuijano <ikerquijano.daw@ciudadescolarfp.es>
2022-04-29 13:08:38 +0200

    Paso 6 Constructor

--- Entornos/Alumno.java
+++ Entornos/Alumno.java
@@ -4,4 +4,11 @@ public class Alumno {
 	private String nombre;
 	private String apellido;
 	private String dni;
+	
+
+	public Alumno(String nom, String ap, String DNI) {
+		this.nombre = nom;
+		this.apellido = ap;
+		this.dni = DNI;
+	}
 }
