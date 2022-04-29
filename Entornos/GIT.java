658987d15c6b462fe229885e76ac1013ef635d9a
IkerQuijano <ikerquijano.daw@ciudadescolarfp.es>
2022-04-29 13:10:38 +0200

    Paso 8 Master

--- Entornos/GIT.java
+++ Entornos/GIT.java
@@ -4,5 +4,8 @@ public class GIT {
 	public static void main(String[] agrs) {
 		System.out.println("Listado de alumnos 1ºDAW.");
 		System.out.println("ALumno 1.");
+		
+		Alumno alumno1 = new Alumno("Iker", "Quijano", "46736462N");
+		System.out.println(alumno1);
 	}
 }
